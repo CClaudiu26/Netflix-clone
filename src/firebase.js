bff3199e8897872c077e8faff3eebf85edeb5708
LeoM1010 <claudiu.cozma28@yahoo.com>
2021-02-18 22:23:53 +0000

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAz7Oj0t4-ml9z05gusz7mTNEt2qpdEbok",
+    authDomain: "netflix-clone-e913f.firebaseapp.com",
+    databaseURL: "https://netflix-clone-e913f.firebaseio.com",
+    projectId: "netflix-clone-e913f",
+    storageBucket: "netflix-clone-e913f.appspot.com",
+    messagingSenderId: "76932738484",
+    appId: "1:76932738484:web:49ad2132b4e6804c3356e1",
+    measurementId: "G-84PEF9XGK1"
+  };
